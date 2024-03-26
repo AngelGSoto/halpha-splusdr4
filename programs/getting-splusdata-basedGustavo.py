b805e71ba3a579047318ceee7694d6e3e5a78b48
AngelGSoto <gsoto.angel@gmail.com>
2024-03-26 18:15:04 -0300

    Some minor changes

--- programs/getting-splusdata-basedGustavo.py
+++ programs/getting-splusdata-basedGustavo.py
@@ -103,10 +103,9 @@ def main(test_mode=False):
         LEFT OUTER JOIN "idr4_psf"."idr4_psf_j0515" AS j0515s ON j0515s.id = det.id
         LEFT OUTER JOIN "idr4_psf"."idr4_psf_j0660" AS j0660s ON j0660s.id = det.id
         LEFT OUTER JOIN "idr4_psf"."idr4_psf_j0861" AS j0861s ON j0861s.id = det.id
-      
-        
+              
         WHERE 
-            r_PStotal > 13 AND r_PStotal <= 16
+            r_PStotal > 17.5 AND r_PStotal <= 18.5
             AND e_r_PStotal <= 0.2 AND e_J0660_PStotal <= 0.2
             AND e_i_PStotal <= 0.2
             AND CLASS_STAR_r > 0.5 AND CLASS_STAR_i > 0.5  
@@ -127,7 +126,7 @@ def main(test_mode=False):
         result = process_field(field, query_template)
         final_table = pd.concat([final_table, result])
 
-    final_table.to_csv(f"iDR4-SPLUS-PStotal-PSF-13r16_class05_flags4.csv", index=False)
+    final_table.to_csv(f"iDR4-SPLUS-PStotal-PSF-175r185_class05_flags4.csv", index=False)
 
 if __name__ == "__main__":
     test_mode = False  # Set this to False when you are ready to run on the entire dataset
