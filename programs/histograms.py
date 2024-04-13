1080f6ba2a3f387516360a08d1a36a4c4721bf68
AngelGSoto <gsoto.angel@gmail.com>
2024-04-12 22:45:19 -0300

    Working in the histoograms

--- programs/histograms.py
+++ programs/histograms.py
@@ -39,22 +39,23 @@ plt.xlabel("r-band Magnitude")
 plt.ylabel("Normalized Density")
 plt.legend()
 plt.grid(True)
-plt.savefig("Disk-histogram-r.pdf")
+plt.savefig("Figs/Disk-histogram-r.pdf")
 
 # Calculate the bin width
 bin_width_d = (mag_range_d[1] - mag_range_d[0]) / num_bins
 
 # Calculate the number density per magnitude bin
-number_density_halpha_d = np.histogram(df_halpha_d["r"], bins=num_bins, range=mag_range_d)[0] / len(df_halpha_d) / bin_width_d / 347.4
-number_density_all_objects_d = np.histogram(df_all_objects_d["r"], bins=num_bins, range=mag_range_d)[0] / len(df_all_objects_d) / bin_width_d / 347.4
+number_density_halpha_d = np.histogram(df_halpha_d["r"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
+number_density_all_objects_d = np.histogram(df_all_objects_d["r"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
 
-# Plot number density per magnitude bin
+# Calculate the fraction of Hα emitters in each magnitude bin
+fraction_halpha = number_density_halpha_d / (number_density_halpha_d + number_density_all_objects_d)
+
+# Plot fraction of Hα emitters in each r-band magnitude bin
 plt.figure(figsize=(10, 6))
-plt.plot(number_density_halpha_d, color='blue', label='Hα Excess (Disk)')
-plt.plot(number_density_all_objects_d, color='red', label='All Disk Stars')
+plt.plot(fraction_halpha, color='blue', label='Fraction of Hα Emitters (Disk)')
 plt.xlabel("Magnitude Bin")
-plt.ylabel("Number Density (mag$^{-1}$ deg$^{-2}$)")
-plt.title("Number Density of Objects in r-band Magnitude Bins")
-plt.legend()
+plt.ylabel("Fraction of Hα Emitters (mag$^{-1}$ deg$^{-2}$)")
 plt.grid(True)
-plt.savefig("Disk-histogram-rpermagdeg.pdf")
+plt.savefig("Figs/Fraction-Halpha-Disk.pdf")
+
