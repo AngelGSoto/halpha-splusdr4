6fde9ab9d9ffe631587405ef4e05d28432debacb
AngelGSoto <gsoto.angel@gmail.com>
2024-04-15 15:48:56 -0300

    Finished the histograms

--- programs/histograms_main.py
+++ programs/histograms_main.py
@@ -10,10 +10,11 @@ import astropy.units as u
 sns.set_theme(style="ticks")
 
 # Open the CSV files
-pattern = 'Disk_Lomeli/*.csv'
+pattern = '*clean.csv'
 csv_files = glob.glob(pattern)
 dfs = []
 
+print(csv_files)
 # Loop through each CSV file and read it into a DataFrame
 for file in csv_files:
     df = pd.read_csv(file)
@@ -23,31 +24,31 @@ for file in csv_files:
 df_all_objects_d = pd.concat(dfs, ignore_index=True)
 
 # The Halpha emitters
-df_halpha_d = pd.read_csv("Ha-emitters-disk/Halpha-disk_splus_Mine_PerField_total.csv")
+df_halpha_d = pd.read_csv("Ha-emitters/Halpha_Mine_PerField_total.csv")
 
 # Define the number of bins and the range of r-band magnitudes
 num_bins = 50
-mag_range_d = (min(df_halpha_d["r"].min(), df_all_objects_d["r"].min()), max(df_halpha_d["r"].max(), df_all_objects_d["r"].max()))
+mag_range_d = (min(df_halpha_d["r_PStotal"].min(), df_all_objects_d["r_PStotal"].min()), max(df_halpha_d["r_PStotal"].max(), df_all_objects_d["r_PStotal"].max()))
 
 # Plot side-by-side histograms of r-band magnitudes for Hα emitters and all objects
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.hist(df_halpha_d["r"], bins=num_bins, range=mag_range_d, alpha=0.7, color='deepskyblue', label='Hα Excess (Disk)', density=True, zorder =3)
-plt.hist(df_all_objects_d["r"], bins=num_bins, range=mag_range_d, alpha=0.7, color='salmon', label='All Disk Stars', density=True, zorder =2)
+plt.hist(df_halpha_d["r_PStotal"], bins=num_bins, range=mag_range_d, alpha=0.7, color='deepskyblue', label='Hα Excess (Main)', density=True, zorder =3)
+plt.hist(df_all_objects_d["r_PStotal"], bins=num_bins, range=mag_range_d, alpha=0.7, color='salmon', label='All Main Stars', density=True, zorder =2)
 plt.xlabel("r-band Magnitude", fontsize=20)
 plt.ylabel("Normalized Density", fontsize=20)
 plt.legend(fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-histogram-r.pdf")
+plt.savefig("Figs/Main-histogram-r.pdf")
 
 # Calculate the bin width
 bin_width_d = (mag_range_d[1] - mag_range_d[0]) / num_bins
 
 # Calculate the number density per magnitude bin
-number_density_halpha_d = np.histogram(df_halpha_d["r"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
-number_density_all_objects_d = np.histogram(df_all_objects_d["r"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
+number_density_halpha_d = np.histogram(df_halpha_d["r_PStotal"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
+number_density_all_objects_d = np.histogram(df_all_objects_d["r_PStotal"], bins=num_bins, range=mag_range_d)[0] / bin_width_d / 347.4
 
 # Calculate the fraction of Hα emitters in each magnitude bin
 fraction_halpha = number_density_halpha_d / (number_density_halpha_d + number_density_all_objects_d)
@@ -56,12 +57,12 @@ fraction_halpha = number_density_halpha_d / (number_density_halpha_d + number_de
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.plot(fraction_halpha, color='navy', label='Fraction of Hα Emitters (Disk)')
+plt.plot(fraction_halpha, color='navy', label='Fraction of Hα Emitters')
 plt.xlabel("Magnitude Bin", fontsize=20)
 plt.ylabel("Fraction of Hα Emitters (mag$^{-1}$ deg$^{-2}$)", fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Fraction-Halpha-Disk.pdf")
+plt.savefig("Figs/Fraction-Halpha-Main.pdf")
 
 # Plot number density of Hα emitters and all objects in each r-band magnitude bin as a bar plot
 plt.figure(figsize=(10, 6))
@@ -72,9 +73,9 @@ plt.tick_params(axis='y', labelsize=20)
 bin_edges = np.arange(len(number_density_halpha_d) + 1) - 0.5
 
 # Plot the histograms with step style
-plt.hist(np.arange(len(number_density_halpha_d)), bins=bin_edges, weights=number_density_halpha_d, color='deepskyblue', label='Hα Excess (Disk)', alpha=1, zorder =3,
+plt.hist(np.arange(len(number_density_halpha_d)), bins=bin_edges, weights=number_density_halpha_d, color='deepskyblue', label='Hα Excess', alpha=1, zorder =3,
          histtype='step', linewidth=3, density=True)
-plt.hist(np.arange(len(number_density_all_objects_d)), bins=bin_edges, weights=number_density_all_objects_d, color='salmon', label='All Disk Stars', alpha=1,  zorder =2,
+plt.hist(np.arange(len(number_density_all_objects_d)), bins=bin_edges, weights=number_density_all_objects_d, color='salmon', label='All Stars', alpha=1,  zorder =2,
          histtype='step', linewidth=3, density=True)
 
 plt.xlabel("r-band Magnitude Bin", fontsize=20)  # Add clarification
@@ -83,33 +84,33 @@ plt.legend(fontsize=20)
 plt.grid(True, axis='y')  # Add grid lines only on the y-axis
 plt.xticks(np.arange(0, len(number_density_halpha_d), 5), rotation=45)  # Set x-ticks to align with the bins and rotate them
 plt.tight_layout()  # Improve spacing between subplots
-plt.savefig("Figs/Number-Density-Disk.pdf")
+plt.savefig("Figs/Number-Density-Main.pdf")
 
 # Plot cumulative distribution function (CDF) of r-band magnitudes
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.hist(df_halpha_d["r"], bins=num_bins, range=mag_range_d, alpha=1, color='deepskyblue', label='Hα Excess (Disk)', cumulative=True, density=True,  zorder =3)
-plt.hist(df_all_objects_d["r"], bins=num_bins, range=mag_range_d, alpha=1, color='salmon', label='All Disk Stars', cumulative=True, density=True,  zorder =2)
+plt.hist(df_halpha_d["r_PStotal"], bins=num_bins, range=mag_range_d, alpha=1, color='deepskyblue', label='Hα Excess', cumulative=True, density=True,  zorder =3)
+plt.hist(df_all_objects_d["r_PStotal"], bins=num_bins, range=mag_range_d, alpha=1, color='salmon', label='All Stars', cumulative=True, density=True,  zorder =2)
 plt.xlabel("r-band Magnitude")
 plt.ylabel("Cumulative Density")
 plt.legend(fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-cdf-r.pdf")
+plt.savefig("Figs/Main-cdf-r.pdf")
 
 # Plot Kernel Density Estimation (KDE) of r-band magnitudes
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-sns.kdeplot(df_halpha_d["r"], color='navy', label='Hα Excess (Disk)', linestyle='-', linewidth=2,  zorder =3)
-sns.kdeplot(df_all_objects_d["r"], color='salmon', label='All Disk Stars', linestyle='--', linewidth=2,  zorder =2)
+sns.kdeplot(df_halpha_d["r_PStotal"], color='navy', label='Hα Excess', linestyle='-', linewidth=2,  zorder =3)
+sns.kdeplot(df_all_objects_d["r_PStotal"], color='salmon', label='All Stars', linestyle='--', linewidth=2,  zorder =2)
 plt.xlabel("r-band Magnitude", fontsize=20)
 plt.ylabel("Density", fontsize=20)
 plt.legend(fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-kde-r.pdf")
+plt.savefig("Figs/Main-kde-r.pdf")
 
 ##############################################################################
 
@@ -119,7 +120,7 @@ coords_d = SkyCoord(ra=df_d_["RA"].values * u.degree, dec=df_d_["DEC"].values * 
 galactic_coords_d = coords_d.galactic
 
 # Calculate Galactic Longitude for Hα emitters
-df_d_h = df_halpha_d.rename(columns={"ALPHA": "RA", "DELTA": "DEC"})
+df_d_h = df_halpha_d.rename(columns={"RA": "RA", "DEC": "DEC"})
 coords_h = SkyCoord(ra=df_d_h["RA"].values * u.degree, dec=df_d_h["DEC"].values * u.degree, frame='icrs')
 galactic_coords_h = coords_h.galactic
 df_halpha_d["GAL_LONG"] = galactic_coords_h.l.deg  # Add Galactic Longitude to the DataFrame
@@ -132,16 +133,16 @@ lon_range = (min(galactic_coords_d.l.deg), max(galactic_coords_d.l.deg))
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.hist(df_halpha_d["GAL_LONG"], bins=num_bins, range=lon_range,  color='deepskyblue', label='Hα Excess (Disk)', alpha=1, zorder=3,
+plt.hist(df_halpha_d["GAL_LONG"], bins=num_bins, range=lon_range,  color='deepskyblue', label='Hα Excess (Main)', alpha=1, zorder=3,
          histtype='step', linewidth=3, density=True)
-plt.hist(galactic_coords_d.l.deg, bins=num_bins, range=lon_range,  color='salmon', label='All Disk Stars', alpha=1, zorder=2,
+plt.hist(galactic_coords_d.l.deg, bins=num_bins, range=lon_range,  color='salmon', label='All Main Stars', alpha=1, zorder=2,
          histtype='step', linewidth=3, density=True)
 plt.xlabel("Galactic Longitude (l)", fontsize=20)
 plt.ylabel("Normalized Density", fontsize=20)
-plt.legend(fontsize=20)
+plt.legend(loc = "upper left", fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-histogram-galactic-longitude.pdf")
+plt.savefig("Figs/Main-histogram-galactic-longitude.pdf")
 
 # Calculate the bin width
 bin_width = (lon_range[1] - lon_range[0]) / num_bins
@@ -157,12 +158,12 @@ fraction_halpha = number_density_halpha_d / (number_density_halpha_d + number_de
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.plot(fraction_halpha, color='navy', label='Fraction of Hα Emitters (Disk)')
+plt.plot(fraction_halpha, color='navy', label='Fraction of Hα Emitters')
 plt.xlabel("Galactic Longitude (l)", fontsize=20)
 plt.ylabel("Fraction of Hα Emitters (mag$^{-1}$ deg$^{-2}$)", fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Fraction-Halpha-Galactic-Longitude.pdf")
+plt.savefig("Figs/Fraction-Halpha-Galactic-Longitude-Main.pdf")
 
 # Plot number density of Hα emitters and all objects in each Galactic Longitude bin as a bar plot
 plt.figure(figsize=(10, 6))
@@ -173,9 +174,9 @@ plt.tick_params(axis='y', labelsize=20)
 bin_edges = np.arange(len(number_density_halpha_d) + 1) - 0.5
 
 # Plot the histograms with step style
-plt.hist(np.arange(len(number_density_halpha_d)), bins=bin_edges, weights=number_density_halpha_d, color='deepskyblue', label='Hα Excess (Disk)', alpha=1, zorder=3,
+plt.hist(np.arange(len(number_density_halpha_d)), bins=bin_edges, weights=number_density_halpha_d, color='deepskyblue', label='Hα Excess', alpha=1, zorder=3,
          histtype='step', linewidth=3, density=True)
-plt.hist(np.arange(len(number_density_all_objects_d)), bins=bin_edges, weights=number_density_all_objects_d, color='salmon', label='All Disk Stars', alpha=1, zorder=2,
+plt.hist(np.arange(len(number_density_all_objects_d)), bins=bin_edges, weights=number_density_all_objects_d, color='salmon', label='All Stars', alpha=1, zorder=2,
          histtype='step', linewidth=3, density=True)
 
 plt.xlabel("Galactic Longitude (l) bin", fontsize=20)  # Add clarification
@@ -184,31 +185,31 @@ plt.legend(fontsize=20)
 plt.grid(True, axis='y')  # Add grid lines only on the y-axis
 plt.xticks(np.arange(0, len(number_density_halpha_d), 5), rotation=45)  # Set x-ticks to align with the bins and rotate them
 plt.tight_layout()  # Improve spacing between subplots
-plt.savefig("Figs/Number-Density-Galactic-Longitude.pdf")
+plt.savefig("Figs/Number-Density-Galactic-Longitude-Main.pdf")
 
 
 # Plot cumulative distribution function (CDF) of Galactic Longitude
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-plt.hist(df_halpha_d["GAL_LONG"], bins=num_bins, range=lon_range, alpha=1, color='deepskyblue', label='Hα Excess (Disk)', cumulative=True, density=True, zorder=3)
-plt.hist(galactic_coords_d.l.deg, bins=num_bins, range=lon_range, alpha=1, color='salmon', label='All Disk Stars', cumulative=True, density=True, zorder=2)
+plt.hist(df_halpha_d["GAL_LONG"], bins=num_bins, range=lon_range, alpha=1, color='deepskyblue', label='Hα Excess', cumulative=True, density=True, zorder=3)
+plt.hist(galactic_coords_d.l.deg, bins=num_bins, range=lon_range, alpha=1, color='salmon', label='All Stars', cumulative=True, density=True, zorder=2)
 plt.xlabel("Galactic Longitude", fontsize=20)
 plt.ylabel("Cumulative Density", fontsize=20)
 plt.legend(fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-cdf-galactic-longitude.pdf")
+plt.savefig("Figs/Main-cdf-galactic-longitude.pdf")
 
 # Plot Kernel Density Estimation (KDE) of Galactic Longitude
 plt.figure(figsize=(10, 6))
 plt.tick_params(axis='x', labelsize=20)
 plt.tick_params(axis='y', labelsize=20)
-sns.kdeplot(df_halpha_d["GAL_LONG"], color='navy', label='Hα Excess (Disk)', linestyle='-', linewidth=2, zorder=3)
-sns.kdeplot(galactic_coords_d.l.deg, color='salmon', label='All Disk Stars', linestyle='--', linewidth=2, zorder=2)
+sns.kdeplot(df_halpha_d["GAL_LONG"], color='navy', label='Hα Excess', linestyle='-', linewidth=2, zorder=3)
+sns.kdeplot(galactic_coords_d.l.deg, color='salmon', label='All Stars', linestyle='--', linewidth=2, zorder=2)
 plt.xlabel("Absolute Galactic Longitude", fontsize=20)
 plt.ylabel("Density", fontsize=20)
 plt.legend(fontsize=20)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("Figs/Disk-kde-galactic-longitude.pdf")
+plt.savefig("Figs/Main-kde-galactic-longitude.pdf")
