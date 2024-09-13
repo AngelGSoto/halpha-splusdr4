5f92f812e73671ff0e6164487fc0df7ddb101704
AngelGSoto <gsoto.angel@gmail.com>
2024-09-13 20:11:05 -0300

    RRlyrae analisys

--- programs/plot_RRlyrae.py
+++ programs/plot_RRlyrae.py
@@ -11,7 +11,7 @@ color_rrlyrae = '#377eb8'  # Dark blue for RR Lyrae stars
 border_color_rrlyrae = 'black'  # Black border for better contrast
 
 # Load data
-df = pd.read_csv("RRlyrae/Tab_2_J_A+A_607_A11_table1-SPLUS_ID_clean-moreParameters.csv")
+df = pd.read_csv("RRlyrae/Tab_2_J_A+A_607_A11_table1-splus-filters-unique.csv")
 
 # Calculate colors
 ri = df["r_PStotal"] - df["i_PStotal"]
@@ -28,7 +28,7 @@ scatter_rrlyrae = ax.scatter(
     ri, rj660,
     color=color_rrlyrae,
     alpha=0.8,
-    s=300,  # Size of points
+    s=350,  # Size of points
     marker='*',  # Star symbol for RR Lyrae
     edgecolors=border_color_rrlyrae,
     linewidths=1.9,
@@ -40,7 +40,7 @@ ax.axhline(y=0.0, color='gray', linestyle='--', linewidth=2, label='r - J0660 = 
 
 # Set limits for axes
 ax.set_xlim(-0.5, 0.6)
-ax.set_ylim(-0.3, 0.7)
+ax.set_ylim(-0.5, 0.7)
 
 # Add minor tick locators without showing the minor ticks
 ax.xaxis.set_minor_locator(MultipleLocator(0.1))
@@ -51,7 +51,7 @@ ax.xaxis.set_minor_formatter(NullFormatter())
 # ax.legend(loc='upper left', fontsize=35)
 
 # Add text with match information
-ax.text(0.5, 0.95, 'RR Lyrae matched with S-PLUS\n(Greer et al. 2017)',
+ax.text(0.5, 0.97, 'RR Lyrae matched with S-PLUS\n(Greer et al. 2017)',
         transform=ax.transAxes, fontsize=25, verticalalignment='top',
         bbox=dict(facecolor='white', alpha=0.7, edgecolor='black'))
 
